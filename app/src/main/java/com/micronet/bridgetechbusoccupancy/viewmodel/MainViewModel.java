9477c0af54da5ef64f045e92d9417d506f6849de
Scott Krstyen <scott.krstyen@micronet-inc.com>
2019-05-16 14:38:38 -0600

    Moved keep alive mechanism

--- app/src/main/java/com/micronet/bridgetechbusoccupancy/viewmodel/MainViewModel.java
+++ app/src/main/java/com/micronet/bridgetechbusoccupancy/viewmodel/MainViewModel.java
@@ -1,25 +1,19 @@
 package com.micronet.bridgetechbusoccupancy.viewmodel;
 
-import static com.micronet.bridgetechbusoccupancy.utils.OutgoingMessage.sendData;
-
 import android.arch.lifecycle.LifecycleOwner;
 import android.arch.lifecycle.Observer;
 import android.arch.lifecycle.ViewModel;
-import android.os.Handler;
 import android.util.SparseArray;
 
 import com.micronet.bridgetechbusoccupancy.repository.Bus;
 import com.micronet.bridgetechbusoccupancy.repository.BusDriver;
 import com.micronet.bridgetechbusoccupancy.repository.Settings;
 
-import com.micronet.bridgetechbusoccupancy.utils.Log;
 import java.util.ArrayList;
 import java.util.List;
 
 public class MainViewModel extends ViewModel {
     private List<Integer> breakAdapter;
-    private Handler keepAliveHandler;
-    private int keepAliveInterval = 60000;
 
     public List<String> breakTypes() {
         List<String> routes = new ArrayList<>();
@@ -31,30 +25,6 @@ public class MainViewModel extends ViewModel {
         }
         return routes;
     }
-
-    public void startKeepAliveMechanism() {
-        keepAliveHandler = new Handler();
-
-        // Every 45 seconds ping the server to keep address up to date.
-        keepAliveHandler.postDelayed(keepAliveUpdate, keepAliveInterval);
-//        Log.d("Bridgetech-KeepAlive", "Started keep alive handler.");
-    }
-
-    public void stopKeepAliveMechanism() {
-        if(keepAliveHandler != null){
-            keepAliveHandler.removeCallbacks(keepAliveUpdate);
-//            Log.d("Bridgetech-KeepAlive", "Stopped keep alive handler.");
-        }
-    }
-
-    private Runnable keepAliveUpdate = new Runnable() {
-        @Override
-        public void run() {
-            sendData();
-//            Log.d("Bridgetech-KeepAlive", "Sent keep alive message.");
-            keepAliveHandler.postDelayed(this, keepAliveInterval);
-        }
-    };
 
     public boolean canLogOut() {
         return !Settings.getInstance().getNoLogout();
